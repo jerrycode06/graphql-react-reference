a12616372ea8646fc12f2771bd6ef7a6f3489a4b
Nikhil Upadhyay <nikhil9690@gmail.com>
2021-02-21 17:28:02 +0530

    Exported schema with dummy data

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,18 @@
 const graphql = require("graphql");
+const _ = require("lodash");
 
-const { GraphQLObjectType, GraphQLString } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+// Dummy Data
+var books = [
+  { name: "Think and Grow Rich", genre: "Inspirational", id: "1" },
+  { name: "Lord of the Rings", genre: "Fantasy", id: "2" },
+  {
+    name: "How to Win Friends and Influence People",
+    genre: "Inspirational",
+    id: "3",
+  },
+];
 
 const BookType = new GraphQLObjectType({
   name: "Book",
@@ -9,4 +21,22 @@ const BookType = new GraphQLObjectType({
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
   }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    book: {
+      type: BookType,
+      args: { id: { type: GraphQLString } },
+      resolve(parent, args) {
+        // code to get data from db or other source
+        return _.find(books, { id: args.id });
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
 });
