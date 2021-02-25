08364f966b49de25ff8cf178cb3bda9e8d6f13a0
Nikhil Upadhyay <nikhil9690@gmail.com>
2021-02-26 01:45:45 +0530

    database connection

--- server/app.js
+++ server/app.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
+const mongoose = require("mongoose");
 
 const app = express();
+
+const username = "jerry_06";
+const password = "nikhil@123";
+
+mongoose.connect(
+  `mongodb+srv://${username}:${password}@cluster0.aaxpv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+
+mongoose.connection.once("open", () => {
+  console.log("Connected to Database");
+});
 
 // bind express with graphql
 app.use(
